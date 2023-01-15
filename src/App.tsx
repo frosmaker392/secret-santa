b38667e2c94a31dbda6bec13eab84a86a1d94d62
Ahmad Zhafir <zzzhafir75@gmail.com>
2023-01-15 21:59:15 +0100

    Setup CI for pocketbase with fly.io
    Add env file

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import AppTemplate from './components/templates/AppTemplate'
 import useUserStore from './hooks/useUserStore'
 import { PocketBaseProvider } from './providers/PocketBaseProvider'
 
-const pb = new PocketBase('http://127.0.0.1:8090')
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL)
 
 const App: Component = () => {
   const onLogout = () => pb.authStore.clear()
